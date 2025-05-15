1279bf6e557dc47fe93fc7870fffc91e5ccd72d4
Andy Horn <andyjhorn@gmail.com>
2025-05-15 16:56:52 -0700

    pass through gridview arguments

--- lib/src/widgets/grid/indexed_drag_target_grid.dart
+++ lib/src/widgets/grid/indexed_drag_target_grid.dart
@@ -1,5 +1,6 @@
 import 'dart:math' show Point;
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:indexed_drag_target/src/shared/constants.dart';
 import 'package:indexed_drag_target/src/shared/shared.dart';
@@ -14,6 +15,21 @@ class IndexedDragTargetGrid<T extends Object> extends StatefulWidget {
     required this.onAccept,
     this.onWillAccept,
     required this.rows,
+    this.childAspectRatio = 1.0,
+    this.clipBehavior = Clip.hardEdge,
+    this.controller,
+    this.crossAxisSpacing = 0.0,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.hitTestBehavior = HitTestBehavior.opaque,
+    this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
+    this.mainAxisSpacing = 0.0,
+    this.padding,
+    this.physics,
+    this.primary,
+    this.restorationId,
+    this.scrollDirection = Axis.vertical,
+    this.semanticChildCount,
+    this.shrinkWrap = false,
   });
 
   final List<IndexedDragTargetGridEntry> children;
@@ -22,6 +38,21 @@ class IndexedDragTargetGrid<T extends Object> extends StatefulWidget {
   final OnAcceptPointCallback<T> onAccept;
   final OnWillAcceptPointCallback<T>? onWillAccept;
   final int rows;
+  final double childAspectRatio;
+  final Clip clipBehavior;
+  final ScrollController? controller;
+  final double crossAxisSpacing;
+  final DragStartBehavior dragStartBehavior;
+  final HitTestBehavior hitTestBehavior;
+  final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
+  final double mainAxisSpacing;
+  final EdgeInsetsGeometry? padding;
+  final ScrollPhysics? physics;
+  final bool? primary;
+  final String? restorationId;
+  final Axis scrollDirection;
+  final int? semanticChildCount;
+  final bool shrinkWrap;
 
   @override
   State<IndexedDragTargetGrid<T>> createState() =>
@@ -37,7 +68,22 @@ class _IndexedDragTargetGridState<T extends Object>
   @override
   Widget build(BuildContext context) {
     return GridView.count(
+      childAspectRatio: widget.childAspectRatio,
+      clipBehavior: widget.clipBehavior,
+      controller: widget.controller,
       crossAxisCount: widget.columns,
+      crossAxisSpacing: widget.crossAxisSpacing,
+      dragStartBehavior: widget.dragStartBehavior,
+      hitTestBehavior: widget.hitTestBehavior,
+      keyboardDismissBehavior: widget.keyboardDismissBehavior,
+      mainAxisSpacing: widget.mainAxisSpacing,
+      padding: widget.padding,
+      physics: widget.physics,
+      primary: widget.primary,
+      restorationId: widget.restorationId,
+      scrollDirection: widget.scrollDirection,
+      semanticChildCount: widget.semanticChildCount,
+      shrinkWrap: widget.shrinkWrap,
       children: [
         for (var i = 0; i < widget.columns * widget.rows; i++) ...[
           DragTarget<T>(
